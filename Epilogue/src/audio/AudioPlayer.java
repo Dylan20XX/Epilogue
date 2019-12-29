e13e0de58f4686c12af6703243b729fe5a26c2e7
account-nine38 <account-nine38@192.168.2.13>
2019-12-28 22:45:53 -0500

    audio adjustments

--- Epilogue/src/audio/AudioPlayer.java
+++ Epilogue/src/audio/AudioPlayer.java
@@ -9,43 +9,43 @@ import javax.sound.sampled.Clip;
 
 public class AudioPlayer {
 
-        // Variables for the music
-        static Clip clip;
-        static boolean mute = false;
-        static BooleanControl muteAudio;
+	// Variables for the music
+	static Clip clip;
+	static boolean mute = false;
+	static BooleanControl muteAudio;
 
-        // Methods that actually create the music
-        public static void playAudio(String audioLocation) {
+	// Methods that actually create the music
+	public static void playAudio(String audioLocation) {
 
-                try {
+		try {
 
-                        // Make the method and pass the variables to the method
-                        File Sound = new File(AudioPlayer.class.getResource(audioLocation).toURI());
-                        AudioInputStream audioInput = AudioSystem.getAudioInputStream(Sound);
-                        clip = AudioSystem.getClip();
-                        clip.open(audioInput);
+			// Make the method and pass the variables to the method
+			File Sound = new File(AudioPlayer.class.getResource(audioLocation).toURI());
+			AudioInputStream audioInput = AudioSystem.getAudioInputStream(Sound);
+			clip = AudioSystem.getClip();
+			clip.open(audioInput);
 
-                        muteAudio = (BooleanControl) clip.getControl(BooleanControl.Type.MUTE);
-                        muteAudio.setValue(mute);
-                        
-                        clip.start();// start the music
+			muteAudio = (BooleanControl) clip.getControl(BooleanControl.Type.MUTE);
+			muteAudio.setValue(mute);
 
-                } catch (Exception ex) {
-                        ex.printStackTrace();
-                }
+			clip.start();// start the music
 
-        }
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
-        // Method to stop the music
-        public static void StopAudio() {
-                // Stop the music
-                clip.stop();
+	}
 
-        }
+	// Method to stop the music
+	public static void StopAudio() {
+		// Stop the music
+		clip.stop();
 
-        public static void changeMute() {
-                mute = !mute;
-                muteAudio.setValue(mute);
-        }
+	}
+
+	public static void changeMute() {
+		mute = !mute;
+		muteAudio.setValue(mute);
+	}
 
 }
